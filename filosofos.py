0a00ab564b85111c714db215d1ae626c808a992c
crltsnch <crltsnch@myuax.com>
2023-05-05 19:50:00 +0200

    constructor

--- filosofos.py
+++ filosofos.py
@@ -10,3 +10,13 @@ class filosofo(threading.Thread):
     estado = []   #lista que almacena el estado de cada filosofo 'PENSANDO', 'HAMBRIENTO' o 'COMIENDO'
     tenedores = []   #lista de semaforos para sincronizar el uso de los tenedores entre los filosofos. Muestra quien esta en cola para uasr el tenedor
     count = 0   #contador que lleva la cuenta del numero de filosofos que se han creado.
+
+    def __init__(self):
+        super().__init__()   #herencia, se llama al constructor de la clase padre ('Threand') usando el método super()
+        self.id = filosofo.count   #se le asigna un id al filosofo de count
+        filosofo.count+=1   #y se incrementa el contador de filosofos
+        filosofo.estado.append('PENSANDO')   #se añade el estado del filosofo a la lista de estados, en estado pensando.
+        filosofo.tenedores.append(threading.Semaphore(0))   #se crea un semaforo para su tenedor izquierdo y se añade a la lista de tenedores
+        print("Filosofo {0} - PENSANDO".format(self.id))   #mensaje en la consolo indicando que el filosofo esta pensando
+    
+    
